4cdd65e66bf701ca52dcd18f6c6f0fb3562d8deb
#bad_sector <64315366+badsector120@users.noreply.github.com>
2022-01-13 20:00:32 +0330

    Update GlobalMembers.py

--- GlobalMembers.py
+++ GlobalMembers.py
@@ -1,30 +1,35 @@
 ﻿import colorama,sys,os,json
 
+
 def main():
     global data
     while True:
         menu()
-        x=input('order :')
-        if x=='1':
+        x = input('order :')
+        if x == '1':
             insert()
-        if x=='2':
+        if x == '2':
             delete()
-        if x=='3':
+        if x == '3':
             update()
-        if x=='4':
+        if x == '4':
             serch()
-        if x=='5':
+        if x == '5':
             try :
                 display()
             except :
                 print("Null")
-        if x=='0':            
+        if x == '0':            
             flush()
             sys.exit(0)
+            
+            
 def flush():
     global data
     with open("file.json", "w") as file:
         json.dump(data, file, indent=4)
+        
+        
 def menu():
     global data
     os.system('cls')
@@ -36,38 +41,41 @@ def menu():
     print('5)-display')
     print('0)-exit')
 
+    
 def insert():
     global data
     print(colorama.Fore.CYAN,'insert',colorama.Fore.WHITE)
     global data
-    frstname=input("Enter frstName :")
-    lastname=input("Enter lastName :")
-    phonenumber=input("Enter phoneNumber (for multi number use \",\" ) :").split(",")
-    tel=input("Enter telPhone (for multi number use \",\" ) :").split(",")
-    Email=input("Enter E-mail (for multi E-mail use \",\" ):").split(",")
-    website=input("Enter webSite (for multi webSite use \",\" ) :").split(",")
-    birthday=input("Enter birthDay :")
+    firstname = input("Enter firstName :")
+    lastname = input("Enter lastName :")
+    phonenumber = input("Enter phoneNumber (for multi number use \",\" ) :").split(",")
+    tel = input("Enter telPhone (for multi number use \",\" ) :").split(",")
+    Email = input("Enter E-mail (for multi E-mail use \",\" ):").split(",")
+    website = input("Enter webSite (for multi webSite use \",\" ) :").split(",")
+    birthday = input("Enter birthDay :")
 
-    k =[i for i in data.keys() if type(i) is int]
+    k = [i for i in data.keys() if type(i) is int]
     if len(k)<1:
-        k=0
+        k = 0
     else:
-        k=max(k)
-        k+=1
-    data[k]={"frstname":frstname,
-             "lastname":lastname,
-             "phoneNumber":phonenumber,
-             "telphone":tel,
-             "Email":Email,
-             "website":website,
-             "birthDay":birthday}
+        k = max(k)
+        k += 1
+    data[k] = {"firstname" : firstname,
+             "lastname" : lastname,
+             "phoneNumber" : phonenumber,
+             "telphone" : tel,
+             "Email" : Email,
+             "website" : website,
+             "birthDay" : birthday}
     flush()
+    
+    
 def delete():
     global data
     print(colorama.Fore.CYAN,'delete',colorama.Fore.WHITE)
-    name=input("Enter name :")
+    name = input("Enter name :")
     for i in data.keys():
-        if data[i]["frstname"]==name:
+        if data[i]["firstname"]==name:
             print(f"{i} ",end=" informaion : ")
             print(data[i])
             k=input("do you wanna to delete this contacte [y/n]")
@@ -80,57 +88,63 @@ def delete():
                 print(f"contacte {i} not deleted.")
     flush()
     input('prosses finshed enter any key to continue...!')
+
+
 def update():
     global data
     print(colorama.Fore.CYAN,'update',colorama.Fore.WHITE)
     name=input("Enter name :")
-    q=True
+    q = False
     for i in data.keys():
-        if data[i]["frstname"]==name:
-            q=False
+        if data[i]["firstname"] == name:
+            q = True
             print(f"{i} -",end=" informaion : ")
             print(data[i])
-            k=input("do you wanna to update this contacte [y/n]")
+            k = input("do you wanna to update this contacte [y/n]")
             if k.lower() == "y":
-                x=input(f"old frstName : [{data[i]['frstname']}] Entre new one or pass")
-                if not x=="pass":
-                    data[i]["frstname"]=x
-                x=input(f"old lastName : [{data[i]['lastname']}] Entre new one or pass")
-                if not x=="pass":
-                    data[i]["lastname"]=x
-                x=input(f"old phoneNumber : {data[i]['phoneNumber']} Entre new one or pass")
-                if not x=="pass":
-                    data[i]["phoneNumber"]=x
-                x=input(f"old telphone : {data[i]['telphone']} Entre new one or pass")
-                if not x=="pass":
-                    data[i]["telphone"]=x
-                x=input(f"old Email : {data[i]['Email']} Entre new one or pass")
-                if not x=="pass":
-                    data[i]["Email"]=x
-                x=input(f"old website : {data[i]['website']} Entre new one or pass")
-                if not x=="pass":
+                x=input(f"old firstName : [{data[i]['firstname']}] Entre new one or pass")
+                if not x == "pass":
+                    data[i]["firstname"] = x
+                x = input(f"old lastName : [{data[i]['lastname']}] Entre new one or pass")
+                if not x == "pass":
+                    data[i]["lastname"] = x
+                x = input(f"old phoneNumber : {data[i]['phoneNumber']} Entre new one or pass")
+                if not x == "pass":
+                    data[i]["phoneNumber"] = x
+                x = input(f"old telphone : {data[i]['telphone']} Entre new one or pass")
+                if not x == "pass":
+                    data[i]["telphone"] = x
+                x = input(f"old Email : {data[i]['Email']} Entre new one or pass")
+                if not x == "pass":
+                    data[i]["Email"] = x
+                x = input(f"old website : {data[i]['website']} Entre new one or pass")
+                if not x == "pass":
                     data[i]["website"]=x
-                x=input(f"old birthDay : [{data[i]['birthDay']}] Entre new one or pass")
-                if not x=="pass":
+                x = input(f"old birthDay : [{data[i]['birthDay']}] Entre new one or pass")
+                if not x == "pass":
                     data[i]["birthDay"]=x
     flush()
     if q:
         input('contacte updated. enter any key to continue...!')
     else:
         input('contacte not found. enter any key to continue...!')
+        
+        
 def serch():
     global data
     print(colorama.Fore.CYAN,'serch',colorama.Fore.WHITE)
-    ke=input("Enter key for search : ")
-    if not ke in ["frstname","lastname","phoneNumber","telphone","Email","website","birthDay"]:
+    ke = input("Enter key for search : ")
+    if not ke in ["firstname","lastname","phoneNumber","telphone","Email","website","birthDay"]:
         input('key dosnt exiset ..!')
         return
-    name=input(f"Enter {ke} : ")
+    name = input(f"Enter {ke} : ")
     for i in data.keys():
-        if data[i][ke]==name:
+        if data[i][ke] == name:
             print(f"pk-{i}-",end=" informaion : ")
             print(data[i])
     input('contacte updated. enter any key to continue...!')
+    
+    
 def display ():
     global data
     print(colorama.Fore.CYAN,'display',colorama.Fore.WHITE)
@@ -138,6 +152,8 @@ def display ():
         print(f"{i} ",end=" informaion : ")
         print(data[i])
     input('press any key to continue...')
+    
+    
 if __name__ == '__main__':
     if os.path.exists("./file.json") :
         with open("file.json", "r") as file:
